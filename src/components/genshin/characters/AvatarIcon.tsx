de62d672a2592b75e25c9dde383969b586cb19c1
Kevin Xiao <xlzqwerty1@gmail.com>
2021-05-10 00:33:30 -0400

    Add rounded border for avatar icons

--- src/components/genshin/characters/AvatarIcon.tsx
+++ src/components/genshin/characters/AvatarIcon.tsx
@@ -1,3 +1,5 @@
+import { SVGProps } from "react"
+
 import clsx from "clsx"
 
 import { avatarIcon, rarityBackground } from "@/assets/static"
@@ -7,6 +9,26 @@ import { AvatarIconProps } from "./icon"
 
 interface FocusedProps {
   isFocused: boolean
+}
+
+// The curvature at the bottom-right of the avatar icon, which overlays on top of the
+// existing character image.
+const SVGRoundBorder = ({
+  className,
+}: SVGProps<SVGSVGElement>): React.ReactElement<SVGSVGElement> => {
+  return (
+    <svg
+      version="1.1"
+      viewBox="0 0 177 32"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="m147 32c17.965-4.5716 27.189-15.211 30-32l0.11165 32h-0.11165z"
+        fill="currentColor" // inherits passed-in text color
+      />
+    </svg>
+  )
 }
 
 const AvatarIcon: React.FC<AvatarIconProps & FocusedProps> = ({
@@ -52,8 +74,9 @@ const AvatarIcon: React.FC<AvatarIconProps & FocusedProps> = ({
               src={avatarIcon(iconName)}
               alt={charName}
             />
+            <SVGRoundBorder className="absolute -bottom-px text-g-paper" />
           </div>
-          <div className="h-6 text-sm leading-6 tracking-tight rounded-b-md font-genshin bg-g-paper text-g-paper-0 2xl:h-7.5 2xl:text-lg 2xl:leading-7.5">
+          <div className="relative h-6 text-sm leading-6 tracking-tight rounded-b-md font-genshin bg-g-paper text-g-paper-0 2xl:h-7.5 2xl:text-lg 2xl:leading-7.5">
             {charName}
           </div>
         </div>
