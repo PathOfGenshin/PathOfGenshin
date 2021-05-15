9f62af61db14e189f53a76a48ce4ea2afe44e29e
Kevin Xiao <xlzqwerty1@gmail.com>
2021-05-15 03:48:45 -0400

    Inline PartyAddCharacterTab

--- src/pages/calculator/party/index.tsx
+++ src/pages/calculator/party/index.tsx
@@ -1,9 +1,108 @@
+import React, { useCallback, useEffect, useState } from "react"
+
+import clsx from "clsx"
+import { useLiveQuery } from "dexie-react-hooks"
+import { noop } from "lodash"
+
+import { GenshinElement, Rarity } from "@/assets/static"
+import AvatarIcon from "@/components/genshin/characters/AvatarIcon"
+import ConfirmationDialog from "@/components/genshin/dialog/ConfirmationDialog"
 import CalculatorLayout from "@/components/layouts/calculator"
 import { ComponentWithLayout } from "@/components/layouts/types"
-import { PartyAddCharacterTab } from "@/components/tabs/PartyAddCharacterTab"
+import { queryAllCharacters } from "@/db"
+import { Character } from "@/generated/model/characters"
+import { useAppDispatch, useAppSelector } from "@/store/hooks"
+import {
+  addCharacter,
+  MAX_PARTY_SIZE,
+  selectCharacterIds,
+} from "@/store/party/partySlice"
+
+interface SelectedCharacter {
+  id: number
+  name: string
+  targetElement: HTMLButtonElement
+}
 
 export const PartyAdd: React.FC & ComponentWithLayout = () => {
-  return <PartyAddCharacterTab />
+  const allCharacters = useLiveQuery(queryAllCharacters)
+  const dispatch = useAppDispatch()
+
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false)
+  const [wantedCharacter, setWantedCharacter] = useState<SelectedCharacter | null>(null)
+
+  const partyIds: number[] = useAppSelector(selectCharacterIds)
+
+  const selectCharacter = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    const charId: number = parseInt(event.currentTarget.dataset["id"])
+    const charName: string = event.currentTarget.dataset["name"]
+    setDialogOpen(true)
+    setWantedCharacter({
+      id: charId,
+      name: charName,
+      targetElement: event.currentTarget,
+    })
+  }
+
+  const addCharacterById = useCallback(() => {
+    dispatch(addCharacter({ id: wantedCharacter.id, name: wantedCharacter.name }))
+  }, [dispatch, wantedCharacter])
+
+  // Clear and unfocus the wanted character
+  useEffect(() => {
+    if (!dialogOpen && wantedCharacter !== null) {
+      wantedCharacter.targetElement.blur()
+    }
+  }, [dialogOpen, wantedCharacter])
+
+  return (
+    <div className="space-y-2 w-full">
+      <div className="font-semibold text-center">
+        {partyIds.length < MAX_PARTY_SIZE ? (
+          <>Select a character to add to your party.</>
+        ) : (
+          <>
+            Your party is now full! Please remove a character from your party before
+            adding another to your party.
+          </>
+        )}
+      </div>
+
+      <div className="mx-auto max-w-5xl">
+        <div
+          className={clsx(
+            "grid gap-4 justify-center transition-opacity duration-1000 grid-cols-auto-icon-6 2xl:grid-cols-auto-icon-8",
+            partyIds.length >= MAX_PARTY_SIZE ? "opacity-50 pointer-events-none" : "",
+          )}
+        >
+          {allCharacters &&
+            allCharacters.map((char: Character) => (
+              <AvatarIcon
+                key={char.id}
+                data-id={char.id}
+                data-name={char.name}
+                charName={char.name}
+                iconName={char.icon}
+                rarity={char.quality as Rarity}
+                element={char.element as GenshinElement}
+                onClick={selectCharacter}
+                isFocused={dialogOpen && (wantedCharacter?.id === char.id ?? false)}
+                disabled={partyIds.includes(char.id)}
+              />
+            ))}
+        </div>
+        <ConfirmationDialog
+          description={`Would you like to add ${wantedCharacter?.name} to your team?`}
+          confirmText="Confirm"
+          cancelText="Cancel"
+          confirmAction={addCharacterById}
+          cancelAction={noop}
+          isOpen={dialogOpen}
+          setIsOpen={setDialogOpen}
+        />
+      </div>
+    </div>
+  )
 }
 
 PartyAdd.Layout = CalculatorLayout
