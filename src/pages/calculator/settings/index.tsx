842f4348c1de8b2b3076b8fcfca8eb43453ef4df
PhaseRush <PhaseAnsible@gmail.com>
2021-06-15 21:08:31 -0700

    Gender toggle in settings (#2)
    * Gender toggle in settings
    
    * please work

--- src/pages/calculator/settings/index.tsx
+++ src/pages/calculator/settings/index.tsx
@@ -1,8 +1,49 @@
+import { useCallback } from "react"
+
+import clsx from "clsx"
+
 import CalculatorLayout from "@/components/layouts/calculator"
 import { ComponentWithLayout } from "@/components/layouts/types"
+import { useAppDispatch, useAppSelector } from "@/store/hooks"
+import {
+  selectTravelerGender,
+  setTravelerGender,
+  TravelerGender,
+} from "@/store/settings/settingsSlice"
+import { Switch } from "@headlessui/react"
 
 export const SettingsPage: React.FC & ComponentWithLayout = () => {
-  return <div className="flex justify-center w-full max-w-5xl">Settings</div>
+  const dispatch = useAppDispatch()
+  const twin: TravelerGender | null = useAppSelector(selectTravelerGender)
+
+  const toggleTwin = useCallback(() => {
+    dispatch(setTravelerGender(twin === "male" ? "female" : "male"))
+  }, [dispatch, twin])
+
+  return (
+    <Switch.Group as="div" className="flex space-x-4">
+      <Switch.Label>
+        Select twin. Currently {twin === "male" || null ? "Aether" : "Lumine"}.
+      </Switch.Label>
+      <Switch
+        as="button"
+        checked={twin === "female"}
+        onChange={toggleTwin}
+        className={clsx(
+          "relative inline-flex flex-shrink-0 h-6 w-12 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer focus:outline-none focus:shadow-outline",
+          twin === "female" ? "bg-pink-400" : "bg-gray-200",
+        )}
+      >
+        {({ checked }) => (
+          <span
+            className={`${
+              checked ? "translate-x-6" : "translate-x-0"
+            } inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full`}
+          />
+        )}
+      </Switch>
+    </Switch.Group>
+  )
 }
 SettingsPage.Layout = CalculatorLayout
 
