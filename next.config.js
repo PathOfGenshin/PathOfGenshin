325750f260e02d7bdc93bf052107c9423f8fdee1
Kevin Xiao <xlzqwerty1@gmail.com>
2021-06-17 22:46:08 -0400

    Upgrade next.js and tailwindcss

--- next.config.js
+++ next.config.js
@@ -16,9 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 module.exports = withPWA({
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
