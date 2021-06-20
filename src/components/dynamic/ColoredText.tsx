5bb90002a6929e84e6ace467336e1512d34992ab
Kevin Xiao <xlzqwerty1@gmail.com>
2021-06-20 17:01:00 -0400

    Use for-iterator in ColoredText parsing

--- src/components/dynamic/ColoredText.tsx
+++ src/components/dynamic/ColoredText.tsx
@@ -32,15 +32,13 @@ const getNextState = (currentState: State, match: RegExpMatchArray): State => {
 export const parseDescription = (desc: string): React.ReactNode[] => {
   const outputs: React.ReactNode[] = []
   const matches = desc.matchAll(DESCRIPTION_REGEX)
-  let result = matches.next()
 
   let state: State = State.STANDARD
-  let s = 0
-  let e = desc.length
-  let c: string | null = null
+  let start = 0
+  let end = desc.length
+  let color: string | null = null
 
-  while (!result.done) {
-    const match: RegExpMatchArray = result.value
+  for (const match of matches) {
     const value: string = match[0]
     const index: number = match.index ?? 0
     const lastIndex: number = index + value.length
@@ -49,32 +47,33 @@ export const parseDescription = (desc: string): React.ReactNode[] => {
     const nextState: State = getNextState(state, match)
 
     // Update previous end index
-    e = index
+    end = index
 
     // Set up for next iteration
     outputs.push(
-      <span key={outputs.length} className={c ? coloredText[`c${c}`] : undefined}>
-        {desc.substring(s, e)}
+      <span
+        key={outputs.length}
+        className={color ? coloredText[`c${color}`] : undefined}
+      >
+        {desc.substring(start, end)}
       </span>,
     )
-    s = lastIndex
-    e = desc.length
+    start = lastIndex
+    end = desc.length
     if (nextState === State.COLOR_START) {
-      c = match[1]
+      color = match[1]
     } else {
-      c = null
+      color = null
     }
     if (nextState === State.NEW_LINE) {
       outputs.push(<br key={outputs.length} />)
     }
     state = nextState
-    result = matches.next()
   }
-
   // End case
   outputs.push(
-    <span key={outputs.length} className={c ? coloredText[`c${c}`] : undefined}>
-      {desc.substring(s, e)}
+    <span key={outputs.length} className={color ? coloredText[`c${color}`] : undefined}>
+      {desc.substring(start, end)}
     </span>,
   )
 
